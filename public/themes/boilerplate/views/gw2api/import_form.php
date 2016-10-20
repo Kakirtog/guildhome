7fd86d0a1ed892e5b8faa0e11ae234f1aa1d0af3
Kakirtog <pimclaessens62@gmail.com>
2016-10-20 18:05:12 +0200

    Update import_form.php

--- public/themes/boilerplate/views/gw2api/import_form.php
+++ public/themes/boilerplate/views/gw2api/import_form.php
@@ -19,8 +19,8 @@
     };
 </script>
 <h3>Import data</h3>
-<p>Please only use the import function as sparsely as possible as it is still in early development and may cause heavy use of the gw2-api.</p>
-<p>I recommend to create an gw2-api key just containing 'account' and 'characters' for now, specially if you have a lot of Characters :)</p>
+<p>Please use the import function as little as possible as it is still in early development and may cause heavy use of the GW2-API.</p>
+<p>I recommend to create a GW2-API key containing just 'account' and 'characters' for now, especially if you have a lot of Characters :)</p>
 <section class="settings form">
     <form method="post" action="{##form_action##}" onsubmit="event.preventDefault(); makerequest(this)">
         <input type="submit" id="submit" name="gw2api_import[submit]" value="{##gw2api_import_submit_text##}" />
